6290ee61f8e62535c6bef79d712c23f28d0255bd
N-Upchurch <nathan@komcreative.com>
2021-03-10 23:14:22 -0600

    Create Die & Speaker components

--- src/App.svelte
+++ src/App.svelte
@@ -21,6 +21,8 @@
   import Phrase from "./Phrase.svelte";
   import StakesBar from "./StakesBar.svelte";
   import EndGame from "./EndGame.svelte";
+  import Speaker from "./Speaker.svelte";
+  import Die from "./Die.svelte";
 
   let formComplete = false;
 
@@ -37,9 +39,10 @@
 
   // Will be set in onMount().
   let currentStage;
+  let numStages = stages.length - 1;
 
   const incrementStage = () => {
-    $stageNumber <= 6 ? ($stageNumber += 1) : (currentStage = 0);
+    $stageNumber <= numStages ? ($stageNumber += 1) : (currentStage = 0);
     $stage = stages[$stageNumber];
   };
 
@@ -51,7 +54,7 @@
   };
 
   const giveUp = () => {
-    $stageNumber = 6;
+    $stageNumber = numStages;
     $stage = stages[$stageNumber];
   };
 
@@ -73,47 +76,7 @@
 
 <main>
   <a id="skipToMain" href="#{$mainContentID}">Skip to main content</a>
-  <svg
-    width="4rem"
-    version="1.1"
-    id="deviceSVG"
-    xmlns="http://www.w3.org/2000/svg"
-    x="0"
-    y="0"
-    viewBox="0 0 84.7 127.4"
-    xml:space="preserve"
-    ><style>
-      .st0 {
-        fill: none;
-        stroke: #fff;
-        stroke-width: 1.5778;
-        stroke-miterlimit: 10;
-      }
-
-      .st1 {
-        fill: #fff;
-      }
-    </style><path
-      class="st0"
-      d="M76.9 118.7c-9.5 1.5-21.7 2.4-35 2.4-12.8 0-24.6-.8-33.8-2.2 1.8 2.3 3.9 4.4 6.3 6.3 8.1.9 17.5 1.4 27.6 1.4 10.6 0 20.4-.6 28.8-1.6 2.2-1.8 4.3-4 6.1-6.3z"
-    />
-    <path
-      class="st0"
-      d="M82.3 75.7c-8.3.6-25.5 1.8-40.3 1.8-14.7 0-31.6-1.2-39.9-1.9L1 94.6c-.2 1.3-.2 2.6-.2 3.9 0 7.6 2.6 14.6 7.2 20.4 9.3 1.4 21.1 2.2 33.8 2.2 13.3 0 25.6-.9 35-2.4 4.4-5.8 7-12.7 7-20.2 0-1.5-.1-2.9-.3-4.3l-1.2-18.5zM57.6 1.8c.5.1 1 .1 1.5.2-.4-.1-.9-.1-1.5-.2zM59.9 2.1l1.5.3-1.5-.3zM62.1 2.5c.5.1.9.2 1.4.3-.4-.1-.9-.2-1.4-.3zM55.2 1.5l2.4.3c-.7-.1-1.5-.2-2.4-.3zM63.6 2.8l2.7.6-2.7-.6zM50.2 1.1c.6 0 1.2.1 1.7.1-.6-.1-1.1-.1-1.7-.1zM74.4 6.6c.4.2.7.5 1 .7-.2-.2-.6-.5-1-.7zM66.8 3.6l1.2.3c-.3 0-.7-.2-1.2-.3z"
-    />
-    <path
-      class="st0"
-      d="M78 11c0 5.7-16.1 10.3-35.9 10.3-19.8 0-35.9-4.6-35.9-10.3l-4 64.6c8.3.7 25.1 1.9 39.9 1.9 14.8 0 31.9-1.2 40.3-1.9L78 11zM73.6 6.2c.3.1.5.3.8.4-.2-.2-.5-.3-.8-.4zM68.6 4.1c.6.2 1.1.4 1.7.6-.5-.2-1.1-.4-1.7-.6zM70.8 4.9c.7.3 1.3.5 1.9.8-.5-.3-1.2-.5-1.9-.8z"
-    />
-    <path
-      class="st0"
-      d="M42.1 21.3C61.9 21.3 78 16.7 78 11c0-1.3-.9-2.6-2.5-3.8-.3-.2-.7-.5-1-.7-.3-.1-.5-.3-.8-.4-.3-.1-.6-.3-.9-.4-.6-.3-1.2-.6-1.9-.8-.2-.1-.3-.1-.5-.2-.5-.2-1.1-.4-1.7-.6-.2 0-.4-.1-.6-.1l-1.2-.3c-.2-.1-.4-.1-.6-.2-.8-.2-1.7-.4-2.7-.6-.5-.1-.9-.2-1.4-.3-.2 0-.5-.1-.7-.1L60 2.2c-.3 0-.5-.1-.8-.1-.5-.1-1-.2-1.5-.2l-2.4-.3-3.3-.3c-.6 0-1.1-.1-1.7-.1C47.7 1 45 .9 42.2.9 22.3.8 6.2 5.4 6.2 11c0 5.7 16.1 10.3 35.9 10.3z"
-    />
-    <ellipse class="st1" cx="32.6" cy="10.7" rx="2.4" ry=".8" />
-    <ellipse class="st1" cx="51.5" cy="10.7" rx="2.4" ry=".8" />
-    <ellipse class="st1" cx="42.1" cy="8" rx="2.4" ry=".8" />
-    <ellipse class="st1" cx="42.1" cy="13.8" rx="2.4" ry=".8" />
-  </svg>
+  <Speaker />
   <h1>Welcome to WireTap Roulette</h1>
 
   {#if $stage === "splash"}
@@ -175,7 +138,7 @@
     <h2 id={$mainContentID}>Thanks for playing!</h2>
     <Card>
       <EndGame points={$score} />
-      <Button buttonText={["try again"]} onClick={[() => initiate(0)]} />
+      <Button buttonText={["try again"]} onClick={[() => location.reload()]} />
     </Card>
   {/if}
 </main>
---
+++ src/Die.svelte
@@ -0,0 +1,181 @@
+<script>
+</script>
+
+<div id="wrapD3Cube">
+  <div id="D3Cube">
+    <div class="side" id="side1">
+      <div class="dot solo" />
+    </div>
+    <div class="side" id="side2">
+      <div class="dotContainer">
+        <div class="dotRow">
+          <div class="dot" />
+        </div>
+        <div class="dotRow right">
+          <div class="dot" />
+        </div>
+      </div>
+    </div>
+    <div class="side" id="side3">
+      <div class="dotContainer">
+        <div class="dotRow">
+          <div class="dot" />
+          <div class="dot" />
+        </div>
+        <div class="dotRow">
+          <div class="dot solo" />
+        </div>
+        <div class="dotRow">
+          <div class="dot" />
+          <div class="dot" />
+        </div>
+      </div>
+    </div>
+    <div class="side" id="side4">
+      <div class="dotContainer">
+        <div class="dotRow">
+          <div class="dot" />
+          <div class="dot" />
+        </div>
+        <div class="dotRow">
+          <div class="dot" />
+          <div class="dot" />
+        </div>
+      </div>
+    </div>
+    <div class="side" id="side5">
+      <div class="dotContainer">
+        <div class="dotRow left">
+          <div class="dot" />
+        </div>
+        <div class="dotRow">
+          <div class="dot solo" />
+        </div>
+        <div class="dotRow right">
+          <div class="dot" />
+        </div>
+      </div>
+    </div>
+    <div class="side" id="side6">
+      <div class="dotContainer">
+        <div class="dotRow">
+          <div class="dot" />
+          <div class="dot" />
+        </div>
+        <div class="dotRow">
+          <div class="dot" />
+          <div class="dot" />
+        </div>
+        <div class="dotRow">
+          <div class="dot" />
+          <div class="dot" />
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<style type="text/css">
+  #wrapD3Cube {
+    width: 250px;
+    height: 213px;
+    margin: 20px auto;
+  }
+  .fadeIn {
+    opacity: 100%;
+    transition: all 0.5s;
+  }
+  .fadeOut {
+    opacity: 0%;
+    transition: all 0.5s;
+  }
+  .dot {
+    background-color: rgba(255, 255, 255, 0.6);
+    width: 1.5rem;
+    height: 1.5rem;
+    border-radius: 100rem;
+  }
+  .solo {
+    margin: auto;
+  }
+  .dotContainer {
+    display: flex;
+    align-items: center;
+    flex-flow: column nowrap;
+    width: 80%;
+    height: 80%;
+  }
+  .dotRow {
+    display: flex;
+    flex-flow: row nowrap;
+    width: 85%;
+    height: 100%;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 0.25rem 0 0.25rem;
+  }
+  .left {
+    justify-content: flex-start;
+  }
+  .right {
+    justify-content: flex-end;
+  }
+  .side {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  #D3Cube {
+    width: 112px;
+    height: 112px;
+    top: 50px;
+    transform-style: preserve-3d;
+    transform: rotateX(-22deg) rotateY(0deg) rotateZ(0deg);
+    margin: auto;
+    position: relative;
+    transform-style: preserve-3d;
+    transition: all 0.5s ease-in-out;
+    animation: cubeRotation 3s infinite ease-in-out alternate;
+  }
+  @keyframes cubeRotation {
+    0% {
+      transform: rotateX(-22deg) rotateY(0deg) rotateZ(0deg);
+    }
+    100% {
+      transform: rotateX(-22deg) rotateY(-730deg) rotateZ(365deg);
+    }
+  }
+  #D3Cube > div {
+    position: absolute;
+    -webkit-transition: all 0.5s ease-in-out;
+    transition: all 0.5s ease-in-out;
+    width: 112px;
+    height: 112px;
+    overflow: hidden;
+    opacity: 0.85;
+  }
+  #side1 {
+    transform: rotatex(90deg) translateX(0px) translateY(0px) translateZ(56px);
+    background-color: rgba(10, 10, 10, 0.7);
+  }
+  #side2 {
+    transform: rotateY(-90deg) translateX(0px) translateY(0px) translateZ(56px);
+    background-color: rgba(0, 0, 0, 0.8);
+  }
+  #side3 {
+    transform: translateX(0px) translateY(0px) translateZ(56px);
+    background-color: rgba(0, 0, 0, 0.8);
+  }
+  #side4 {
+    transform: rotateY(90deg) translateX(0px) translateY(0px) translateZ(56px);
+    background-color: rgba(0, 0, 0, 0.8);
+  }
+  #side5 {
+    transform: rotateY(180deg) translateX(0px) translateY(0px) translateZ(56px);
+    background-color: rgba(0, 0, 0, 0.8);
+  }
+  #side6 {
+    transform: rotateX(-90deg) translateX(0px) translateY(0px) translateZ(56px);
+    background-color: rgba(0, 0, 0, 0.8);
+  }
+</style>
