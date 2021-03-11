997eb19b1ca45c3e95fa1d63ff0d5b24b0ec20e3
N-Upchurch <nathan@komcreative.com>
2021-03-10 21:14:06 -0600

    Add insructions

--- src/Instructions.svelte
+++ src/Instructions.svelte
@@ -1,7 +1,16 @@
 <script>
 </script>
 
-<p>
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae corporis
-  magni illo laboriosam amet fuga explicabo, nesciunt adipisci laborum nam.
-</p>
+<p>Welcome to Wiretap Roulette! Here's how to play:</p>
+<ol>
+  <li>
+    Make sure your smart speaker is within earshot of your device, and that
+    voice recognition is turned off on your smart speaker.
+  </li>
+  <li>Click roll to send a random commend to your smart speaker.</li>
+  <li>If you chicken out, tap "give up" to see your score and end the game.</li>
+  <li>
+    If you didn't panic and cancel the command, tap "did it" to rack up some
+    sweet, sweet points and continue the game.
+  </li>
+</ol>
